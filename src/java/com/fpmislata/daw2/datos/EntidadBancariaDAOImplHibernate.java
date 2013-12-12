5af2254edc293979fb8042c3e967d620fb158ab3
alumno <alumno@2A5PC20>
2013-12-12 11:47:14 +0100

    commit2 tavira

---
+++ src/java/com/fpmislata/daw2/datos/EntidadBancariaDAOImplHibernate.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fpmislata.daw2.datos;
+
+import com.fpmislata.daw2.modelo.EntidadBancaria;
+import java.io.Serializable;
+
+
+/**
+ *
+ * @author alumno
+ */
+public class EntidadBancariaDAOImplHibernate extends GenericDAOImplHibernate<EntidadBancaria, Integer> implements EntidadBancariaDAO{
+
+       
+}
